be017c71e85bac6e0e6d553df40d893492132397
gor-arnaud <arnaud.michoud@gameofroom.fr>
2023-10-23 10:53:13 +0200

    Tasks lists with component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul v-if="afficher">
+      <li v-for="(tache, index) in taches" :key="index">
+        <my-task :text="tache.nom" :done="tache.fait" @remove="onTaskRemove(index)" @check="onTaskCheck(index)">
+        </my-task>
+      </li>
+    </ul>
+    <button v-on:click="afficher = true" v-if="!afficher" class="button">
+      Afficher vos tâches
+    </button>
+    <button v-on:click="afficher = false" v-else class="button">
+      Masquer vos tâches
+    </button>
+    <br />
+    <label>Nouvelle tâche</label><input type="text" v-model="nouvelleTache" class="input">
+    <button class="button is-success" v-on:click="ajouterTache">
+      <span>Nouvelle tâche</span>
+    </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyTask from './components/MyTask.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyTask
+  },
+  data() {
+    return {
+      taches: [
+        { nom: 'faire les courses', fait: false },
+        { nom: 'étendre la lessive', fait: true },
+        { nom: 'faire un top 1 à Fortnite', fait: false },
+      ],
+      afficher: true,
+      nouvelleTache: '',
+    }
+  },
+  methods: {
+    ajouterTache() {
+      this.taches.push({ nom: this.nouvelleTache, fait: false });
+      this.nouvelleTache = '';
+    },
+    onTaskRemove(index) {
+      this.taches.splice(index, 1);
+    },
+    onTaskCheck(index) {
+      this.taches[index].fait = !this.taches[index].fait;
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/MyTask.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="task-layout">
+        <span :class="{ 'tache-faite': done }">{{ text }}</span>
+        <input type="checkbox" :checked="done" @click="onCheck" class="checkbox">
+        <button class="button is-danger is-outlined" v-on:click="onRemoveClick">
+            <span>Supprimer</span>
+        </button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'MyTask',
+    props: {
+        text: {
+            type: String,
+            default: ''
+        },
+        done: {
+            type: Boolean,
+            default: false
+        }
+    },
+    emits: ['check', 'remove'],
+    methods: {
+        onRemoveClick() {
+            this.$emit('remove');
+        },
+        onCheck() {
+            this.$emit('check');
+        }
+    }
+}
+</script>
+<style scoped>
+.tache-faite {
+    text-decoration: line-through;
+}
+
+.task-layout {
+    display: grid;
+    grid-template-columns: 1fr auto auto;
+}
+</style>
