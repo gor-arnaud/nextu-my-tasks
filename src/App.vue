95f393e6034cfb2304a2db8b641a968727bb1598
matteolvt <146728526+matteolvt@users.noreply.github.com>
2024-01-22 21:24:37 +0100

    rendu_exo_Matteo

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,16 @@
     <ul v-if="afficher">
       <li v-for="(tache, index) in taches" :key="index">
         <ul>
-          <my-task v-bind:text="tache.nom" v-bind:done="tache.fait" @remove="onTaskRemove(index)" v-on:check="onTaskCheck(index, $event)">
+          <my-task 
+          v-bind:text="tache.nom" 
+          v-bind:done="tache.fait" 
+          @remove="onTaskRemove(index)" 
+          v-on:check="onTaskCheck(index, $event)">
           </my-task>
         </ul>
       </li>
     </ul>
+    <sentece :taches="taches"></sentece>
     <button v-on:click="afficher = true" v-if="!afficher" class="button">
       Afficher vos tâches
     </button>
@@ -16,20 +21,25 @@
       Masquer vos tâches
     </button>
     <br />
-    <label>Nouvelle tâche</label><input type="text" v-model="nouvelleTache" class="input">
+    <label>Nouvelle tâche</label><input type="text" v-model="nouvelleTache" class="input" @input="clearMessageError">
     <button class="button is-success" v-on:click="ajouterTache">
       <span>Nouvelle tâche</span>
     </button>
+    <div v-if="messageError" class="message-error">{{ messageError }}</div>
   </div>
 </template>
 
 <script>
 import MyTask from './components/MyTask.vue';
+import Sentece from './components/Sentece.vue';
+
+
 
 export default {
   name: 'App',
   components: {
-    MyTask
+    MyTask,
+    Sentece
   },
   data() {
     return {
@@ -40,12 +50,17 @@ export default {
       ],
       afficher: true,
       nouvelleTache: '',
+      messageError: '', 
     }
   },
   methods: {
     ajouterTache() {
-      this.taches.push({ nom: this.nouvelleTache, fait: false });
-      this.nouvelleTache = '';
+      if (this.nouvelleTache.trim() === ''){
+        this.messageError = "Give task's name"; 
+        return; 
+      }
+      this.taches.push({nom: this.nouvelleTache, fait: false});
+      this.nouvelleTache =''; 
     },
     onTaskRemove(index) {
       this.taches.splice(index, 1);
@@ -53,6 +68,11 @@ export default {
     onTaskCheck(index, event) {
       this.taches[index].fait = !this.taches[index].fait;
       console.log('check event: ', event);
+    },
+    clearMessageError() {
+      if (this.messageError) {
+        this.messageError = '';
+      }
     }
   }
 }
---
+++ src/components/Sentece.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="total">
+        You finished {{ tacheFini }} task<span v-if="tacheFini !== 1">s</span> on {{ tacheTotal }}.
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'Sentece',
+        props: {
+            taches: {
+                type: Array,
+                requiered: true,
+            }
+        },
+        computed: {
+        tacheTotal() {
+            return this.taches.length;
+            }, 
+        tacheFini() {
+            return this.taches.filter (tache => tache.fait).length; 
+            }
+        }
+    
+    }
+</script>
